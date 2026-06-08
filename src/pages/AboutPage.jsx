365169050cb1faa7bff75308b8ce998f933d0c0e
yuezhengwang66 <wyz15905@gmail.com>
2026-06-08 14:05:09 -0500

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
   return (
     <main className="content-page">
       <div className="container">
-        <Link className="back-link" to="/">HOME</Link>
+        <Link className="back-link" to="/">Home</Link>
       </div>
       <About />
     </main>
